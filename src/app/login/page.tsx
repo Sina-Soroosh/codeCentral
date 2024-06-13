8b0e8100f38d953bc47f706487af2c297485ce49
Sina-Soroosh <sinasoroosh07@gmail.com>
2024-06-13 10:16:12 +0330

    Create login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,12 @@
+import { Metadata } from "next";
+import React from "react";
+
+export const metadata: Metadata = {
+  title: "ورود به حساب کاربری - مرکز کد",
+};
+
+function page() {
+  return <></>;
+}
+
+export default page;
