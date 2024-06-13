ba18617c246b44f0a3974041faca838143dd9e5b
Sina-Soroosh <sinasoroosh07@gmail.com>
2024-06-13 11:02:09 +0330

    Create register page

---
+++ src/app/register/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "ثبت نام - مرکز کد",
+};
+
+function page() {
+  return <></>;
+}
+
+export default page;
