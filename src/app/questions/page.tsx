cf9d6e4a5e8c5cf6a74d58feb7431e594237171e
Sina-Soroosh <sinasoroosh07@gmail.com>
2024-06-13 11:37:27 +0330

    Coding Main template for questions

--- src/app/questions/page.tsx
+++ src/app/questions/page.tsx
@@ -1,12 +1,17 @@
-import { Metadata } from "next";
 import React from "react";
+import { Metadata } from "next";
+import Main from "@/components/templates/Questions/Main/Main";
 
 export const metadata: Metadata = {
   title: "سوالات - مرکز کد",
 };
 
 function page() {
-  return <></>;
+  return (
+    <>
+      <Main />
+    </>
+  );
 }
 
 export default page;
---
+++ src/components/templates/Questions/Main/Main.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./Main.module.css";
+
+function Main() {
+  return (
+    <>
+      <div className={styles.main}>
+        <div className={styles.content}></div>
+      </div>
+    </>
+  );
+}
+
+export default Main;
