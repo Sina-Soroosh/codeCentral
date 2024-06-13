9c3748eff3f0ccd4ad6fb0d7cf881818bcb47e8a
Sina-Soroosh <sinasoroosh07@gmail.com>
2024-06-13 10:10:16 +0330

    Coding Metadata for home page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,13 @@ import Footer from "@/components/modules/Footer/Footer";
 import Header from "@/components/modules/Header/Header";
 import "@/styles/custom.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  icons: {
+    icon: "/images/logo/favicon.png",
+  },
+};
 
 export default function RootLayout({
   children,
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,9 @@
 import Main from "@/components/templates/Home/Main/Main";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "صفحه اصلی - مرکز کد",
+};
 
 export default function Home() {
   return (
