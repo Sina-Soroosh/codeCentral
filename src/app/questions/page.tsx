fbb737dc42937490c8aa545e5930eddf31c0d847
Sina-Soroosh <sinasoroosh07@gmail.com>
2024-06-13 11:29:44 +0330

    Create questions page

---
+++ src/app/questions/page.tsx
@@ -0,0 +1,12 @@
+import { Metadata } from "next";
+import React from "react";
+
+export const metadata: Metadata = {
+  title: "سوالات - مرکز کد",
+};
+
+function page() {
+  return <></>;
+}
+
+export default page;
