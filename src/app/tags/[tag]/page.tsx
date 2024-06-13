fbc01d720a1f03fd40a99b120040bdc7171cf783
Sina-Soroosh <sinasoroosh07@gmail.com>
2024-06-13 14:29:37 +0330

    Create tags/[tag] page

---
+++ src/app/tags/[tag]/page.tsx
@@ -0,0 +1,12 @@
+import { Metadata } from "next";
+import React from "react";
+
+export const metadata: Metadata = {
+  title: "سوالات جاوااسکریپت - مرکز کد",
+};
+
+function page() {
+  return <></>;
+}
+
+export default page;
