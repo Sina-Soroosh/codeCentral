9112d31460b353b360d8fe4ab887a1c88c4f370f
Sina-Soroosh <sinasoroosh07@gmail.com>
2024-06-15 17:38:14 +0330

    Coding Main template for about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { Metadata } from "next";
+import Main from "@/components/templates/About/Main/Main";
 
 export const metadata: Metadata = {
   title: "درباره مرکز کد - مرکز کد",
 };
 
 function page() {
-  return <></>;
+  return (
+    <>
+      <Main />
+    </>
+  );
 }
 
 export default page;
