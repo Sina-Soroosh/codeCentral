9b7918e31568bc4eb357f72c3378e3b381a4bc65
Sina-Soroosh <sinasoroosh07@gmail.com>
2024-06-15 17:10:29 +0330

    Coding Main template for rules

--- src/app/rules/page.tsx
+++ src/app/rules/page.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { Metadata } from "next";
+import Main from "@/components/templates/Rules/Main/Main";
 
 export const metadata: Metadata = {
   title: "قوانین - مرکز کد",
 };
 
 function page() {
-  return <></>;
+  return (
+    <>
+      <Main />
+    </>
+  );
 }
 
 export default page;
