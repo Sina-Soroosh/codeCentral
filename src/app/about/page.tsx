19089212340acde282f56c745aaad02a3cfa37f6
Sina-Soroosh <sinasoroosh07@gmail.com>
2024-06-15 17:26:27 +0330

    Create about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "درباره مرکز کد - مرکز کد",
+};
+
+function page() {
+  return <></>;
+}
+
+export default page;
