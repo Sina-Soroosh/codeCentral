18fa3e9c57d34aa33ef641b51f13f972e600bba1
Sina-Soroosh <sinasoroosh07@gmail.com>
2024-06-15 18:43:36 +0330

    Create not-found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "صفحه مورد نظر یافت نشد - مرکز کد",
+};
+
+function NotFound() {
+  return <></>;
+}
+
+export default NotFound;
