544f289bff9f3ba8a97e5ffdaa743420a739bc4c
Sina-Soroosh <sinasoroosh07@gmail.com>
2024-06-16 09:09:06 +0330

    Create questions/[id] page

---
+++ src/app/questions/[id]/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "چرا پاسخ false است؟ - مرکز کد",
+};
+
+function page() {
+  return <></>;
+}
+
+export default page;
