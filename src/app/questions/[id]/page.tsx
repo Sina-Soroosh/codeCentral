ad3d45c59dc7ebc31a9b286171a8ec6a381ad1ea
Sina-Soroosh <sinasoroosh07@gmail.com>
2024-06-16 09:47:45 +0330

    Coding Main template for questions/[id]

--- src/app/questions/[id]/page.tsx
+++ src/app/questions/[id]/page.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { Metadata } from "next";
+import Main from "@/components/templates/Questions/Question/Main/Main";
 
 export const metadata: Metadata = {
   title: "چرا پاسخ false است؟ - مرکز کد",
 };
 
 function page() {
-  return <></>;
+  return (
+    <>
+      <Main />
+    </>
+  );
 }
 
 export default page;
---
+++ src/components/templates/Questions/Question/Main/Main.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import styles from "./Main.module.css";
+import Link from "next/link";
+
+function Main() {
+  return (
+    <>
+      <div className={styles.main}>
+        <div className={styles.content}>
+          <div className={styles.question}>
+            <div className={styles.title}>
+              <h3>
+                <Link href="/questions/کاربرد-هوک">چرا پاسخ false است؟</Link>
+              </h3>
+            </div>
+            <div className={styles.body}>
+              <pre>
+                let x = [1,2,3]
+                <br />
+                let y =[1,2,3]
+                <br />
+                <br />
+                console.log(x===y)
+              </pre>
+            </div>
+            <div className={styles.tags}>
+              <Link href="/tags/react" className={styles.tag}>
+                react
+              </Link>
+              <Link href="/tags/js" className={styles.tag}>
+                js
+              </Link>
+            </div>
+            <div className={styles.user}>
+              <span>
+                در تاریخ 1402/03/22 توسط <Link href="/user/test">test</Link>
+              </span>
+            </div>
+          </div>
+          <div className={styles.answers}></div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Main;
