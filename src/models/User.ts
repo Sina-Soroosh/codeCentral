8343d727ecd7e06331fb8627e78c6ff9899d2b35
Sina-Soroosh <sinasoroosh07@gmail.com>
2024-06-18 15:04:26 +0330

    Coding User model

---
+++ src/models/User.ts
@@ -0,0 +1,39 @@
+import mongoose from "mongoose";
+
+const schema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      index: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      index: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["ADMIN", "USER"],
+      default: "USER",
+      required: true,
+    },
+    refreshToken: {
+      type: String,
+      required: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const model = mongoose.models.User || mongoose.model("User", schema);
+
+export default model;
