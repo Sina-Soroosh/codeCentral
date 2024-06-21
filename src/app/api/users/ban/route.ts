086b9692ffd8946ac5ed03b7f0002c655b8dc65e
Sina-Soroosh <sinasoroosh07@gmail.com>
2024-06-21 12:25:57 +0330

    Coding ban api for DELETE method

--- src/app/api/users/ban/route.ts
+++ src/app/api/users/ban/route.ts
@@ -4,9 +4,15 @@ import BanModel from "@/models/Ban";
 import UserModel from "@/models/User";
 import { User } from "@/types/Users.types";
 import patterns from "@/utils/patterns";
+import { ObjectId } from "mongoose";
 import { NextRequest } from "next/server";
 
 type Body = {
+  email: string;
+};
+
+type BanUser = {
+  _id: ObjectId;
   email: string;
 };
 
@@ -58,6 +64,56 @@ export const POST = async (req: NextRequest): Promise<Response> => {
       { message: "Ban user is successfully !!" },
       { status: 201 }
     );
+  } catch (error) {
+    return Response.json(
+      { message: "Internal Server Error !!", error },
+      { status: 500 }
+    );
+  }
+};
+
+export const DELETE = async (req: NextRequest): Promise<Response> => {
+  try {
+    await connectToDB();
+
+    const applicant = await getUser();
+
+    if (!applicant.isLogin || !applicant.isAdmin) {
+      return Response.json(
+        { message: "You do not access to this data !!" },
+        {
+          status: 403,
+        }
+      );
+    }
+
+    const body: Body = await req.json();
+
+    if (!patterns.email.test(body.email)) {
+      return Response.json(
+        { message: "Params is invalid !!" },
+        {
+          status: 403,
+        }
+      );
+    }
+
+    const banUser: null | BanUser = await BanModel.findOne({
+      email: body.email,
+    }).lean();
+
+    if (!banUser) {
+      return Response.json(
+        { message: "Notfound ban !!" },
+        {
+          status: 404,
+        }
+      );
+    }
+
+    await BanModel.deleteOne({ _id: banUser._id });
+
+    return Response.json({ message: "remove ban is successfully !!" });
   } catch (error) {
     return Response.json(
       { message: "Internal Server Error !!", error },
