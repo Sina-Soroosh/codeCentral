20e15c86192b641a666b651b6c72610050b7bee3
Sina-Soroosh <sinasoroosh07@gmail.com>
2024-06-24 11:42:12 +0330

    Connect p-user/activity page with BackEnd

--- src/app/p-user/activity/page.tsx
+++ src/app/p-user/activity/page.tsx
@@ -6,6 +6,10 @@ import Main from "@/components/templates/PanelUser/Activity/Main/Main";
 import SetCookie from "@/components/modules/SetCookie/SetCookie";
 import getUser from "@/helpers/getUserServer";
 import { redirect } from "next/navigation";
+import { Answer } from "@/types/Answer.types";
+import { QuestionWithoutBody } from "@/types/Question.types";
+import AnswerModel from "@/models/Answer";
+import QuestionModel from "@/models/Question";
 
 type ActivityProps = {
   searchParams: SearchParams;
@@ -22,11 +26,53 @@ async function page({ searchParams }: ActivityProps) {
     redirect("/login");
   }
 
+  let allActivity: (Answer | QuestionWithoutBody)[] = [];
+
+  if (searchParams.tab === "answers") {
+    allActivity = await AnswerModel.find(
+      { user: user.user._id },
+      "_id createdAt question"
+    )
+      .populate({
+        path: "question",
+        select: "user title tags createdAt answersCount shortName",
+        populate: [
+          { path: "user", select: "username" },
+          { path: "tags", select: "shortName title" },
+        ],
+      })
+      .sort({ createdAt: "desc" });
+  } else {
+    allActivity = await QuestionModel.find(
+      { user: user.user._id },
+      "user title tags createdAt answersCount shortName"
+    )
+      .populate([
+        { path: "user", select: "username" },
+        { path: "tags", select: "shortName title" },
+      ])
+      .sort({ createdAt: "desc" });
+  }
+
+  let selectedActivity: (Answer | QuestionWithoutBody)[] = [
+    ...JSON.parse(JSON.stringify(allActivity)),
+  ];
+
+  let endIndex = (+searchParams.page || 1) * 5;
+  let startIndex = endIndex - 5;
+
+  selectedActivity = selectedActivity.slice(startIndex, endIndex);
+
   return (
     <>
       {user.token ? <SetCookie token={user.token} /> : null}
       <UserPanel>
-        <Main searchParams={searchParams} />
+        <Main
+          searchParams={searchParams}
+          isAnswers={searchParams.tab === "answers"}
+          activity={selectedActivity}
+          manyPage={Math.ceil(allActivity.length / 5)}
+        />
       </UserPanel>
     </>
   );
--- src/components/templates/PanelUser/Activity/Main/Main.tsx
+++ src/components/templates/PanelUser/Activity/Main/Main.tsx
@@ -4,12 +4,17 @@ import Tab from "@/components/modules/Tab/Tab";
 import QuestionBox from "@/components/modules/QuestionBox/QuestionBox";
 import Pagination from "@/components/modules/Pagination/Pagination";
 import { SearchParams } from "@/types/SearchParams.types";
+import { Answer } from "@/types/Answer.types";
+import { QuestionWithoutBody } from "@/types/Question.types";
 
 type MainProps = {
   searchParams: SearchParams;
+  isAnswers: boolean;
+  activity: (Answer | QuestionWithoutBody)[];
+  manyPage: number;
 };
 
-function Main({ searchParams }: MainProps) {
+function Main({ searchParams, isAnswers, activity, manyPage }: MainProps) {
   return (
     <>
       <div className={styles.main}>
@@ -27,15 +32,33 @@ function Main({ searchParams }: MainProps) {
               searchParams={searchParams}
               isUser
             />
-            <QuestionBox />
-            <QuestionBox />
-            <QuestionBox />
-            <Pagination
-              activePage={isNaN(+searchParams?.page) ? 1 : +searchParams.page}
-              manyPage={3}
-              path={`/p-user/activity`}
-              searchParams={searchParams}
-            />
+            {activity.length ? (
+              <>
+                {isAnswers
+                  ? activity.map((answer) => (
+                      <QuestionBox
+                        key={answer._id.toString()}
+                        {...(answer as Answer).question}
+                      />
+                    ))
+                  : activity.map((question) => (
+                      <QuestionBox
+                        key={question._id.toString()}
+                        {...(question as QuestionWithoutBody)}
+                      />
+                    ))}
+                <Pagination
+                  activePage={
+                    isNaN(+searchParams?.page) ? 1 : +searchParams.page
+                  }
+                  manyPage={manyPage}
+                  path={`/p-user/activity`}
+                  searchParams={searchParams}
+                />
+              </>
+            ) : (
+              <p className={styles.err}>موردی یافت نشد !</p>
+            )}
           </div>
         </div>
       </div>
