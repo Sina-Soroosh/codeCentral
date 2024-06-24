5b6cf39284cc67716bfc8e7ed610adb3de839002
Sina-Soroosh <sinasoroosh07@gmail.com>
2024-06-24 12:27:42 +0330

    Connect user/[username] page with BackEnd

--- src/app/user/[username]/page.tsx
+++ src/app/user/[username]/page.tsx
@@ -2,6 +2,13 @@ import React from "react";
 import { SearchParams } from "@/types/SearchParams.types";
 import { Metadata } from "next";
 import Main from "@/components/templates/User/Main/Main";
+import { User } from "@/types/Users.types";
+import UserModel from "@/models/User";
+import { redirect } from "next/navigation";
+import { QuestionWithoutBody } from "@/types/Question.types";
+import { Answer } from "@/types/Answer.types";
+import AnswerModel from "@/models/Answer";
+import QuestionModel from "@/models/Question";
 
 type UserProps = {
   searchParams: SearchParams;
@@ -14,10 +21,63 @@ export function generateMetadata({ params }: UserProps): Metadata {
   };
 }
 
-function page({ params, searchParams }: UserProps) {
+async function page({ params, searchParams }: UserProps) {
+  const user: null | (User & { createdAt: Date; bio: string }) =
+    await UserModel.findOne({
+      username: params.username,
+    }).lean();
+
+  if (!user) {
+    return redirect("/404");
+  }
+
+  let allActivity: (Answer | QuestionWithoutBody)[] = [];
+
+  if (searchParams.tab === "answers") {
+    allActivity = await AnswerModel.find(
+      { user: user._id },
+      "_id createdAt question"
+    )
+      .populate({
+        path: "question",
+        select: "user title tags createdAt answersCount shortName",
+        populate: [
+          { path: "user", select: "username" },
+          { path: "tags", select: "shortName title" },
+        ],
+      })
+      .sort({ createdAt: "desc" });
+  } else {
+    allActivity = await QuestionModel.find(
+      { user: user._id },
+      "user title tags createdAt answersCount shortName"
+    )
+      .populate([
+        { path: "user", select: "username" },
+        { path: "tags", select: "shortName title" },
+      ])
+      .sort({ createdAt: "desc" });
+  }
+
+  let selectedActivity: (Answer | QuestionWithoutBody)[] = [
+    ...JSON.parse(JSON.stringify(allActivity)),
+  ];
+
+  let endIndex = (+searchParams.page || 1) * 5;
+  let startIndex = endIndex - 5;
+
+  selectedActivity = selectedActivity.slice(startIndex, endIndex);
+
   return (
     <>
-      <Main searchParams={searchParams} username={params.username} />
+      <Main
+        searchParams={searchParams}
+        username={params.username}
+        user={user}
+        isAnswers={searchParams.tab === "answers"}
+        activity={selectedActivity}
+        manyPage={Math.ceil(allActivity.length / 5)}
+      />
     </>
   );
 }
--- src/components/templates/User/Main/Main.tsx
+++ src/components/templates/User/Main/Main.tsx
@@ -5,13 +5,21 @@ import { SearchParams } from "@/types/SearchParams.types";
 import Tab from "@/components/modules/Tab/Tab";
 import QuestionBox from "@/components/modules/QuestionBox/QuestionBox";
 import Pagination from "@/components/modules/Pagination/Pagination";
+import { User } from "@/types/Users.types";
+import { convertToSolarDate } from "@/helpers/date";
+import { QuestionWithoutBody } from "@/types/Question.types";
+import { Answer } from "@/types/Answer.types";
 
 type MainProps = {
   searchParams: SearchParams;
   username: string;
+  manyPage: number;
+  user: User & { createdAt: Date; bio: string };
+  isAnswers: boolean;
+  activity: (Answer | QuestionWithoutBody)[];
 };
 
-function Main({ searchParams, username }: MainProps) {
+function Main(props: MainProps) {
   return (
     <>
       <div className={styles.main}>
@@ -22,37 +30,55 @@ function Main({ searchParams, username }: MainProps) {
                 <FaUser />
               </div>
               <div className={styles.info}>
-                <span>Sina-Soroosh</span>
-                <span>تاریخ عضویت : 1403/03/20</span>
+                <span>{props.user.username}</span>
+                <span>
+                  تاریخ عضویت :{convertToSolarDate(props.user.createdAt)}
+                </span>
               </div>
             </div>
             <div className={`${styles.bio} col-md-6`}>
               <h5>بیوگرافی کاربر</h5>
-              <p>
-                برنامه نویس فرانت اند هستم و با ریکت کار می‌کنم. <br /> عاشق
-                چالش‌های فرانت‌اند و برنامه نویسی هستم و دوست دارم کنار
-                توسعه‌های پروژه دانش خود را ارتقا بدم.
-              </p>
+              <p dangerouslySetInnerHTML={{ __html: props.user.bio }}></p>
             </div>
           </div>
           <div className={styles.activity}>
             <Tab
               activeTab={
-                searchParams?.tab === "answers" ? "answers" : "questions"
+                props.searchParams?.tab === "answers" ? "answers" : "questions"
               }
-              path={`/user/${username}`}
-              searchParams={searchParams}
+              path={`/user/${props.username}`}
+              searchParams={props.searchParams}
               isUser
             />
-            <QuestionBox />
-            <QuestionBox />
-            <QuestionBox />
-            <Pagination
-              activePage={isNaN(+searchParams?.page) ? 1 : +searchParams.page}
-              manyPage={3}
-              path={`/user/${username}`}
-              searchParams={searchParams}
-            />
+            {props.activity.length ? (
+              <>
+                {props.isAnswers
+                  ? props.activity.map((answer) => (
+                      <QuestionBox
+                        key={answer._id.toString()}
+                        {...(answer as Answer).question}
+                      />
+                    ))
+                  : props.activity.map((question) => (
+                      <QuestionBox
+                        key={question._id.toString()}
+                        {...(question as QuestionWithoutBody)}
+                      />
+                    ))}
+                <Pagination
+                  activePage={
+                    isNaN(+props.searchParams?.page)
+                      ? 1
+                      : +props.searchParams.page
+                  }
+                  manyPage={props.manyPage}
+                  path={`/user/${props.user.username}`}
+                  searchParams={props.searchParams}
+                />
+              </>
+            ) : (
+              <p className={styles.err}>موردی یافت نشد !</p>
+            )}
           </div>
         </div>
       </div>
