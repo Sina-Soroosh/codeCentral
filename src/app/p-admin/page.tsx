e77efa240c3f5a45ad540aca6c2918afb542b16e
Sina-Soroosh <sinasoroosh07@gmail.com>
2024-06-26 10:08:57 +0330

    Create p-admin page

--- src/app/p-admin/page.tsx
+++ src/app/p-admin/page.tsx
@@ -1,7 +1,27 @@
 import React from "react";
 import AdminPanel from "@/components/layout/AdminPanel/AdminPanel";
+import getUser from "@/helpers/getUserServer";
+import { redirect } from "next/navigation";
+import { connectToDB } from "@/configs/db";
+import { Metadata } from "next";
 
-function page() {
+export const metadata: Metadata = {
+  title: "پنل مدیریت - مرکز کد",
+};
+
+async function page() {
+  await connectToDB();
+
+  const user = await getUser();
+
+  if (user.isLogin === false) {
+    redirect("/login");
+  }
+
+  if (user.isAdmin === false) {
+    redirect("/p-user");
+  }
+
   return (
     <>
       <AdminPanel></AdminPanel>
