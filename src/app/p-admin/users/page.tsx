66946501ec4719417608298e3387548533081409
Sina-Soroosh <sinasoroosh07@gmail.com>
2024-06-27 11:57:24 +0330

    Create p-admin/users page

---
+++ src/app/p-admin/users/page.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import AdminPanel from "@/components/layout/AdminPanel/AdminPanel";
+import { connectToDB } from "@/configs/db";
+import getUser from "@/helpers/getUserServer";
+import { redirect } from "next/navigation";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: " لیست کاربران پنل مدیریت - مرکز کد",
+};
+
+async function page() {
+  await connectToDB();
+
+  const user = await getUser();
+
+  if (user.isLogin === false) {
+    redirect("/login");
+  }
+
+  if (user.isAdmin === false) {
+    redirect("/p-user");
+  }
+
+  return (
+    <>
+      <AdminPanel></AdminPanel>
+    </>
+  );
+}
+
+export default page;
